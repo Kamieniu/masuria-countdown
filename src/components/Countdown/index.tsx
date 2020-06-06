4345959458e6c9219a2e95ead9a9d6f97c65ce8b
Franciszek Stodulski <franek.stodulski@gmail.com>
2020-06-06 16:32:58 +0200

    initial

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+const Countdown = () => {
+  const calculateTimeLeft = (): {
+    hours: number;
+    seconds: number;
+    minutes: number;
+    days: number;
+  } => {
+    const difference = +new Date('2020-07-12') - +new Date();
+
+    return difference > 0
+      ? {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        }
+      : {
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  return (
+    <div className="flex items-center p-10 text-center justify-center bg-white mt-4 rounded-15">
+      <span className="text-4xl ml-3">{timeLeft.days}</span>
+      <span className="text-xl opacity-75 ml-3">days,</span>
+      <span className="text-4xl ml-3">{timeLeft.hours}</span>
+      <span className="text-xl opacity-75 ml-3">hours,</span>
+      <span className="text-4xl ml-3">{timeLeft.minutes}</span>
+      <span className="text-xl opacity-75 ml-3">minutes,</span>
+      <span className="text-4xl ml-3">{timeLeft.seconds}</span>
+      <span className="text-xl opacity-75">seconds,</span>
+    </div>
+  );
+};
+
+export default Countdown;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        '15': '15px',
+        '20': '20px',
+      },
+      color: {
+        blacky: '#393e46',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};

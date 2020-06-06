4d7afc7d46a14a5bce5701c70febd0a4ff978b2c
Franciszek Stodulski <franek.stodulski@gmail.com>
2020-06-06 16:30:54 +0200

    Updates

---
+++ precache-manifest.b4e3b9496406f1ea96da5414af4c5a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3601bf1de0780d914d7bb12229948bd4",
+    "url": "/masuria-countdown/index.html"
+  },
+  {
+    "revision": "32cb3fa2ec9dd15543c5",
+    "url": "/masuria-countdown/static/css/main.95464269.chunk.css"
+  },
+  {
+    "revision": "d5b4054c121572adecc6",
+    "url": "/masuria-countdown/static/js/2.7859b7b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/masuria-countdown/static/js/2.7859b7b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cb3fa2ec9dd15543c5",
+    "url": "/masuria-countdown/static/js/main.7cbc96ff.chunk.js"
+  },
+  {
+    "revision": "6d03d888ce38349c3316",
+    "url": "/masuria-countdown/static/js/runtime-main.af61a7aa.js"
+  },
+  {
+    "revision": "6c8137e34e36f1ba204f6c8f372779e6",
+    "url": "/masuria-countdown/static/media/background.6c8137e3.jpg"
+  }
+]);

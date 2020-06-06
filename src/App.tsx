fe4300a9bb7690378b58296bf14200dc9863cd5a
Franciszek Stodulski <franek.stodulski@gmail.com>
2020-06-06 16:42:38 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ const App = () => {
   return (
     <main className="h-screen w-screen flex items-center ">
       <figure className="absolute top-0  w-full h-full overflow-hidden left-0 flex items-center justify-center">
-        <img src={imageSrc} alt="" />
+        <img className="w-full h-full object-cover" src={imageSrc} alt="" />
       </figure>
 
       <div className="container mx-auto relative flex flex-col items-center">
-        <div className="px-10 py-5 text-center bg-white rounded-15 shadow-md">
-          <h1 className="text-5xl">Masuria countdown</h1>
+        <div className="px-10 py-3 md:py-5 text-center bg-white rounded-15 shadow-md">
+          <h1 className="text-sm md:text-5xl">Masuria countdown</h1>
         </div>
         <Countdown />
 
         <a
-          className="mt-6 px-6 py-4 bg-blue-200 shadow-md rounded-15 hover:bg-blue-400 duration-200 ease-in-out text-white"
+          className="mt-6 px-3 md:px-6 py-2 md:py-4 bg-blue-200 shadow-md rounded-15 hover:bg-blue-400 duration-200 ease-in-out text-white text-sm md:text-md"
+          target="_blank"
           href="https://www.airbnb.pl/rooms/40576035"
         >
           Check this out!
--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -33,15 +33,21 @@ const Countdown = () => {
   });
 
   return (
-    <div className="flex items-center p-10 text-center justify-center bg-white mt-4 rounded-15">
-      <span className="text-4xl ml-3">{timeLeft.days}</span>
-      <span className="text-xl opacity-75 ml-3">days,</span>
-      <span className="text-4xl ml-3">{timeLeft.hours}</span>
-      <span className="text-xl opacity-75 ml-3">hours,</span>
-      <span className="text-4xl ml-3">{timeLeft.minutes}</span>
-      <span className="text-xl opacity-75 ml-3">minutes,</span>
-      <span className="text-4xl ml-3">{timeLeft.seconds}</span>
-      <span className="text-xl opacity-75">seconds,</span>
+    <div className="flex items-center p-3 md:p-10 text-center justify-center bg-white mt-4 rounded-15">
+      <span className="text-md md:text-4xl ml-1 md:ml-3">{timeLeft.days}</span>
+      <span className="text-md md:text-xl opacity-75 ml-1 md:ml-3">days,</span>
+      <span className="text-md md:text-4xl ml-1 md:ml-3">{timeLeft.hours}</span>
+      <span className="text-md md:text-xl opacity-75 ml-1 md:ml-3">hours,</span>
+      <span className="text-md md:text-4xl ml-1 md:ml-3">
+        {timeLeft.minutes}
+      </span>
+      <span className="text-md md:text-xl opacity-75 ml-1 md:ml-3">
+        minutes,
+      </span>
+      <span className="text-md md:text-4xl ml-1 md:ml-3">
+        {timeLeft.seconds}
+      </span>
+      <span className="text-md md:text-xl opacity-75">seconds,</span>
     </div>
   );
 };

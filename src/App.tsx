96ba2533c237094d304818a3ae0a7294d5ee40d1
Franciszek Stodulski <franek.stodulski@gmail.com>
2020-06-06 16:25:11 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import imageSrc from './background.jpg';
+import Countdown from './components/Countdown';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <main className="h-screen w-screen flex items-center ">
+      <figure className="absolute top-0  w-full h-full overflow-hidden left-0 flex items-center justify-center">
+        <img src={imageSrc} alt="" />
+      </figure>
+
+      <div className="container mx-auto relative flex flex-col items-center">
+        <div className="px-10 py-5 text-center bg-white rounded-15 shadow-md">
+          <h1 className="text-5xl">Masuria countdown</h1>
+        </div>
+        <Countdown />
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="mt-6 px-6 py-4 bg-blue-200 shadow-md rounded-15 hover:bg-blue-400 duration-200 ease-in-out text-white"
+          href="https://www.airbnb.pl/rooms/40576035"
         >
-          Learn React
+          Check this out!
         </a>
-      </header>
-    </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
